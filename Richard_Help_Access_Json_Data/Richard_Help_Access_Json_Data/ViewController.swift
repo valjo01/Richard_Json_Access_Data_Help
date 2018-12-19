f59d3d6fac8e8f62b5ef617591b284fd17a851e6
valjo01 <34964327+valjo01@users.noreply.github.com>
2018-12-19 11:19:11 +0100

    First

---
+++ Richard_Help_Access_Json_Data/Richard_Help_Access_Json_Data/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Richard_Help_Access_Json_Data
+//
+//  Created by Valentin on 19.12.18.
+//  Copyright © 2018 Novaheal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
